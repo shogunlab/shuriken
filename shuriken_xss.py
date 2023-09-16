ccebd7c3a9676d1f1dbeaa884cbba909352829a9
Steven Patterson <shogunlab@gmail.com>
2023-09-15 20:53:52 -0400

    Update shuriken_xss.py for Python 3

--- shuriken_xss.py
+++ shuriken_xss.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 """Set Python environment."""
 # -*- coding: utf-8 -*-
 
@@ -12,7 +12,6 @@ import time
 from splinter import Browser
 from fuzzywuzzy import fuzz
 
-
 class Color:
     """Use colors to make command line output prettier."""
 
@@ -22,15 +21,13 @@ class Color:
     BLUE = '\033[94m'
     END = '\033[0m'
 
-
 class Shuriken:
     """Object for testing lists of XSS payloads."""
 
     def __init__(self):
         """Initiate object with default encoding and other required data."""
         # Expect some weird characters from fuzz lists, make encoding UTF-8
-        reload(sys)
-        sys.setdefaultencoding('utf8')
+        sys.getdefaultencoding()
 
         # All potential XSS findings
         self.xss_links = []
@@ -50,35 +47,35 @@ class Shuriken:
     def main(self):
         """Call functions to inject XSS and test for vulnerabilities."""
         # Print out a welcome message
-        print "\n"
-        print "=" * 34 + Color.YELLOW + "\nWelcome to the" + Color.RED + \
+        print("\n")
+        print("=" * 34 + Color.YELLOW + "\nWelcome to the" + Color.RED + \
             " Shuriken " + Color.YELLOW + "XSS tool!\n" + \
-            Color.END + "=" * 34 + "\n"
+            Color.END + "=" * 34 + "\n")
 
         self.test_xss(self.user_args.PAYLOADS_LIST,
                       self.user_args.URL,
                       self.user_args.REQUEST_DELAY,
                       self.user_args.SCREENSHOT_NAME)
-        print Color.GREEN + "\n=== Testing complete! ===\n" + Color.END
+        print(Color.GREEN + "\n=== Testing complete! ===\n" + Color.END)
 
         # If the test found possible XSS vulnerabilities, ask if we should log
         if self.xss_links:
-            print Color.YELLOW + \
+            print(Color.YELLOW + \
                 "XSS vulnerabilities were detected!" + \
-                Color.END
+                Color.END)
             self.log_file(self.xss_links)
         # There were partials detected by fuzzy detection
         elif self.xss_partials:
-            print Color.YELLOW + \
+            print(Color.YELLOW + \
                 "Partial XSS vulnerabilities were detected!" + \
-                Color.END
+                Color.END)
             self.log_file(self.xss_links)
         else:
-            print Color.YELLOW + \
+            print(Color.YELLOW + \
                 "No potential XSS vulnerabilities detected...\n" + \
                 Color.END
-            print "Goodbye!"
-            print "\n"
+            )
+            print("Goodbye!\n")
 
     def make_sure_path_exists(self, path):
         """Ensure that file path exists before writing."""
@@ -103,8 +100,8 @@ class Shuriken:
             try:
                 time.sleep(float(request_delay))
             except KeyboardInterrupt:
-                print Color.YELLOW + \
-                    "\nTesting interrupted by user!\n" + Color.END
+                print(Color.YELLOW + \
+                    "\nTesting interrupted by user!\n" + Color.END)
                 self.log_file(self.xss_links)
                 sys.exit()
 
@@ -134,8 +131,8 @@ class Shuriken:
         fuzzy_level = self.user_args.FUZZY_DETECTION
 
         if payload.lower() in browser_object.html.lower():
-            print Color.GREEN + "\n[+] XSS vulnerability found:" + \
-                Color.END
+            print(Color.GREEN + "\n[+] XSS vulnerability found:" + \
+                Color.END)
 
             # If user set the --screen flag to target, capture screenshot of
             # payload
@@ -145,20 +142,20 @@ class Shuriken:
 
             # Add link to list of all positive XSS hits
             self.xss_links.append(injected_link)
-            print Color.BLUE + injected_link + Color.END
+            print(Color.BLUE + injected_link + Color.END)
         # If user enabled fuzzy detection and partial score was larger than
         # fuzz level, add it to partials list and print results
         elif fuzzy_level and (partial_score >= fuzzy_level):
-            print Color.YELLOW + \
-                "\n[-] Partial XSS vulnerability found:" + Color.END
-            print Color.BLUE + injected_link + Color.END
+            print(Color.YELLOW + \
+                "\n[-] Partial XSS vulnerability found:" + Color.END)
+            print(Color.BLUE + injected_link + Color.END)
             self.xss_partials.append(injected_link)
-            print "Detection score: %s" % partial_score
+            print("Detection score: %s" % partial_score)
         else:
-            print Color.RED + "\n[+] No XSS detected at: \n" + \
-                Color.BLUE + injected_link + Color.END
+            print(Color.RED + "\n[+] No XSS detected at: \n" + \
+                Color.BLUE + injected_link + Color.END)
             if (fuzzy_level):
-                print "Detection score: %s" % partial_score
+                print("Detection score: %s" % partial_score)
 
     def take_screenshot(self, user_screenshot_name, browser_object,
                         screen_index):
@@ -171,16 +168,16 @@ class Shuriken:
             "_" + screen_index + ".png"
         # Save screenshot to directory
         browser_object.driver.save_screenshot(screenshot_file_name)
-        print Color.YELLOW + "Screenshot saved: " + \
-            screenshot_file_name + Color.END
+        print(Color.YELLOW + "Screenshot saved: " + \
+            screenshot_file_name + Color.END)
 
     def test_xss(self, payloads_param, link, request_delay,
                  user_screenshot_name):
         """Load string from payload list and call function to inject it."""
         # If the user added time delay, show them what it is set to.
         if request_delay is not None:
-            print Color.YELLOW + "\n[!] Request delay is set to [" + \
-                str(request_delay) + "] seconds between requests." + Color.END
+            print(Color.YELLOW + "\n[!] Request delay is set to [" + \
+                str(request_delay) + "] seconds between requests." + Color.END)
 
         # Load the payload file and inject all payloads
         # into user supplied URL to test for XSS
@@ -196,10 +193,10 @@ class Shuriken:
     def log_file(self, link_list):
         """Log successful XSS payload reflections to file."""
         # Prompt the user to confirm log file, if yes, log XSS hits
-        log_confirm = raw_input(
+        log_confirm = input(
             "\nWould you like to save these results? [y/n] > ")
         if log_confirm == "y":
-            target_name = raw_input("Please enter the target name > ")
+            target_name = input("Please enter the target name > ")
             # Check if logs directory exists, if not then create it
             self.make_sure_path_exists("logs")
             # Set file name
@@ -227,12 +224,12 @@ class Shuriken:
                         "\n*** Created from the payload file >>> " +
                         self.user_args.PAYLOADS_LIST)
                     partial_file.close()
-            print "\nFile successfully saved as: " + \
-                Color.BLUE + file_name + Color.END
-            print "\n"
+            print("\nFile successfully saved as: " + \
+                Color.BLUE + file_name + Color.END)
+            print("\n")
         else:
-            print "\nGoodbye!"
-            print "\n"
+            print("\nGoodbye!")
+            print("\n")
 
     def parse_args(self):
         """Parse arguments from the user sent on command line."""
@@ -258,21 +255,20 @@ class Shuriken:
 
         # Check for existence of '{xss}' injection point in URL string
         if "{xss}" not in arguments.URL:
-            print Color.RED + "Please provide the '{xss}' placeholder for" + \
-                " injection point in the URL" + Color.END
-            print Color.GREEN + \
+            print(Color.RED + "Please provide the '{xss}' placeholder for" + \
+                " injection point in the URL" + Color.END)
+            print(Color.GREEN + \
                 "Example: -u \"http://example.com/index.php?name={xss}\"" + \
-                Color.END
+                Color.END)
             exit()
 
         return arguments
-
 
 if __name__ == "__main__":
     try:
         Shuriken().main()
     except KeyboardInterrupt:
-        print Color.YELLOW + \
-            "\nTesting interrupted by user!\n" + Color.END
+        print(Color.YELLOW + \
+            "\nTesting interrupted by user!\n" + Color.END)
         Shuriken().log_file(Shuriken().xss_links)
         sys.exit()
